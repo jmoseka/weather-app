fc3b35a9e3058f6d826afeb65eee03028c4ecc19
jmoseka <jamylam3@gmail.com>
2022-06-21 10:52:15 +0300

    Add basic styling

--- src/Components/Home/WeatherCard/Card.js
+++ src/Components/Home/WeatherCard/Card.js
@@ -1,8 +1,67 @@
 import React from 'react';
+import './Card.css';
+import { WiHumidity } from 'react-icons/wi';
+import { FaTemperatureHigh } from 'react-icons/fa';
+import { RiWindyFill } from 'react-icons/ri';
+import { BsSunrise, BsSunset } from 'react-icons/bs';
 
 function Card() {
   return (
-    <div>Card</div>
+    <div>
+      <div className="card">
+        <div className="card-body-2">
+          <ul className="weather-list">
+            <li>
+              <div className="card-icon"><FaTemperatureHigh /></div>
+              <p>Temperature</p>
+            </li>
+            <li>
+              <div className="card-icon"><WiHumidity /></div>
+              <p>Humidity</p>
+
+            </li>
+            <li className="card-icon">
+              <div className="card-icon"><RiWindyFill /></div>
+              <p>Windy</p>
+
+            </li>
+          </ul>
+
+          <div className="sun-display">
+            <div className="sunrise">
+              <p>Sunrise</p>
+              <BsSunrise />
+            </div>
+            <div className="sunset">
+              <p>Sunset</p>
+              <BsSunset />
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <section className="cities">
+        <div className="card-country">
+          Kamplala
+        </div>
+        <div className="card-country">
+          Dar-Es-Salaam
+        </div>
+        <div className="card-country">
+          Kigali
+        </div>
+        <div className="card-country">
+          Bunjumbura
+        </div>
+        <div className="card-country">
+          Kinshasa
+        </div>
+        <div className="card-country">
+          Addis Ababa
+        </div>
+      </section>
+    </div>
   );
 }
 
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import './Home.css';
 import Card from './WeatherCard/Card';
+import './Home.css';
 
 function Home() {
   return (
     <div className="home">
       <div className="main-head">
         <div className="main-time">
-          <h1 className="region">Nairobi</h1>
-          <h2 className="date">September 7</h2>
+          <div>
+            <h1 className="region">Nairobi</h1>
+            <h2 className="date">September 7</h2>
+          </div>
         </div>
-
         <div className="search-box">
           <input className="search-input" placeholder="Search city" />
         </div>
@@ -18,20 +19,6 @@ function Home() {
 
       <Card />
 
-      <div className="list-countries">
-        <div>
-          Kampala
-        </div>
-        <div>
-          Kigali
-        </div>
-        <div>
-          Dar-Es-Salaam
-        </div>
-        <div>
-          Kinshasa
-        </div>
-      </div>
     </div>
   );
 }
