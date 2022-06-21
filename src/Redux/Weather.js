e56e2ac90dd839a69d48efb642c3dd2df19b6888
jmoseka <jamylam3@gmail.com>
2022-06-21 12:43:47 +0300

    add recuder

--- src/Redux/Weather.js
+++ src/Redux/Weather.js
@@ -1,32 +1,36 @@
-export const fetchBook = () => async (dispatch) => {
-    try {
-      const response = await axios.get(url);
-      const data = await response.data;
-      const books = [];
-      Object.entries(data).forEach((el) => {
-        books.push({
-          item_id: el[0],
-          ...el[1][0],
-        });
-      });
-      dispatch(getBook(books));
-    } catch (error) {
-      <h2>{error}</h2>;
-    }
-  };
+import axios from 'axios';
 
-  export default function weatherReducer(state = [], action) {
-    switch (action.type) {
-      case GET_BOOK:
-        return action.payload;
-      case ADD_BOOK:
-        return [
-          ...state,
-          action.book,
-        ];
-      case DELETE_BOOK:
-        return [...state].filter((item) => item.item_id !== action.id);
-      default:
-        return state;
-    }
+const GET_DATA = 'weather-app/weatherReducer/getData';
+
+const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=-1.28333&lon=36.81667&exclude=hourly,daily&appid=5709f5315143352b497276f1f19fb6bc';
+
+export const getData = (payload) => ({
+  type: GET_DATA,
+  payload,
+});
+
+export const fetchBook = () => async () => {
+  const response = await axios.get(url);
+  const data = await response.data;
+  console.log(data);
+  /* const weatherData = [];
+    Object.entries(data).forEach((el) => {
+      weatherData.push({
+        item_id: el[0],
+        ...el[1][0],
+      });
+    });
+    dispatch(getBook(books));
+  } catch (error) {
+    <h2>{error}</h2>;
+  } */
+};
+
+export default function weatherReducer(state = [], action) {
+  switch (action.type) {
+    case GET_DATA:
+      return action.payload;
+    default:
+      return state;
   }
+}
