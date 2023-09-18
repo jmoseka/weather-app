293c2985f10d82116482328a0bf98abead67ae2d
Jamila Moseka <jamylam3@gmail.com>
2023-09-18 10:21:04 +0300

    Update App.js add home path

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="*" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/detail" element={<Detail />} />
         </Routes>
       </BrowserRouter>
